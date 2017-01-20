1094e334bd5d6601c4caf48120c29b85f3d215cd
YAP! <GMADDA200@caledonian.ac.uk>
2017-01-20 23:45:43 +0000

    Entire Game. So far.

---
+++ GameJamGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public string[] controls = new string[2];
+    /*
+     * 0 = PlayerHorizontal
+     * 1 = PlayerVertical
+     */
+
+    Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	    if (Input.GetAxis(controls[0]) > 0)
+        {
+
+            //transform.position += new Vector3 (0.1f, 0.0f, 0.0f);
+            transform.localEulerAngles -= new Vector3(0.0f, 0.0f, 2.0f);
+
+        }
+        else if (Input.GetAxis(controls[0]) < 0)
+        {
+
+            //transform.position -= new Vector3 (0.1f, 0.0f, 0.0f);
+            transform.localEulerAngles += new Vector3(0.0f, 0.0f, 2.0f);
+
+        }
+
+
+        float angle = (transform.eulerAngles.z + 90)* Mathf.Deg2Rad;
+        if (Input.GetAxis(controls[1]) < 0 && rb2D.velocity.magnitude < 10)
+        {
+
+            print ("hello");
+
+            //transform.position += new Vector3(Mathf.Cos(angle) * 0.1f, Mathf.Sin(angle) * 0.1f, 0.0f);
+            rb2D.AddForce (transform.up * 5);
+
+        }
+
+    }
+
+}
---
+++ GameJamGame/Assets/Scripts/TentacleMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TentacleMovement : MonoBehaviour
+{
+
+    public string[] controls = new string[2];
+    /*
+     * 0 = Horizontal
+     * 1 = PlayerVertical
+     */
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetAxis(controls[0]) > 0)
+        {
+
+            transform.position += new Vector3 (0.1f, 0.0f, 0.0f);
+
+        }
+        else if (Input.GetAxis(controls[0]) < 0)
+        {
+
+            transform.position -= new Vector3 (0.1f, 0.0f, 0.0f);
+
+        }
+
+        if (Input.GetAxis(controls[1]) > 0)
+        {
+
+            transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+
+        }
+        else if (Input.GetAxis(controls[1]) < 0)
+        {
+
+            transform.position -= new Vector3(0.0f, 0.1f, 0.0f);
+
+        }
+
+    }
+
+}
