e3ec4226ef38124018b8ae658740d27055ebcc2a
YAP! <GMADDA200@caledonian.ac.uk>
2017-01-21 21:23:16 +0000

    Jazzy Sun

---
+++ GameJamGame/Assets/Scripts/JazzySun.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JazzySun : MonoBehaviour {
+
+    float countdown;
+    public float maxCountdown;
+
+	// Use this for initialization
+	void Start ()
+    {
+        countdown = maxCountdown;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (countdown > 0)
+            countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+
+            if (transform.eulerAngles.z == 0)
+                transform.eulerAngles = new Vector3 (0, 0, -17);
+            else
+                transform.eulerAngles = new Vector3 (0, 0, 0);
+
+            countdown = maxCountdown;
+        }
+
+	}
+
+}
