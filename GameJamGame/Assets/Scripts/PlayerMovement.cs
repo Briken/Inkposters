259f6c049f5ca474185751317828efc0af7fb916
Briken <brokenbriken@gmail.com>
2017-01-21 21:04:52 +0000

    stun
    sun in

--- GameJamGame/Assets/Scripts/PlayerMovement.cs
+++ GameJamGame/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
 
+    public bool stunned = false;
+    public float stunDur = 3;
+
     public string[] controls = new string[2];
     /*
      * 0 = PlayerHorizontal
@@ -21,32 +24,41 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-
-	    if (Input.GetAxis(controls[0]) > 0)
+        if (!stunned)
         {
+            if (Input.GetAxis(controls[0]) > 0)
+            {
 
-            //transform.position += new Vector3 (0.1f, 0.0f, 0.0f);
-            transform.localEulerAngles -= new Vector3(0.0f, 0.0f, 4.0f);
+                //transform.position += new Vector3 (0.1f, 0.0f, 0.0f);
+                transform.localEulerAngles -= new Vector3(0.0f, 0.0f, 4.0f);
 
+            }
+            else if (Input.GetAxis(controls[0]) < 0)
+            {
+
+                //transform.position -= new Vector3 (0.1f, 0.0f, 0.0f);
+                transform.localEulerAngles += new Vector3(0.0f, 0.0f, 4.0f);
+
+            }
+
+
+            float angle = (transform.eulerAngles.z + 90) * Mathf.Deg2Rad;
+            if (Input.GetAxis(controls[1]) < 0 && rb2D.velocity.magnitude < 20)
+            {
+
+                //transform.position += new Vector3(Mathf.Cos(angle) * 0.1f, Mathf.Sin(angle) * 0.1f, 0.0f);
+                rb2D.AddForce(transform.up * 10);
+
+            }
         }
-        else if (Input.GetAxis(controls[0]) < 0)
+        if (stunned)
         {
-
-            //transform.position -= new Vector3 (0.1f, 0.0f, 0.0f);
-            transform.localEulerAngles += new Vector3(0.0f, 0.0f, 4.0f);
-
+            StartCoroutine(Stun());
         }
-
-
-        float angle = (transform.eulerAngles.z + 90)* Mathf.Deg2Rad;
-        if (Input.GetAxis(controls[1]) < 0 && rb2D.velocity.magnitude < 20)
-        {
-
-            //transform.position += new Vector3(Mathf.Cos(angle) * 0.1f, Mathf.Sin(angle) * 0.1f, 0.0f);
-            rb2D.AddForce (transform.up * 10);
-
-        }
-
     }
-
+    IEnumerator Stun()
+    {
+        yield return new WaitForSeconds(stunDur);
+        stunned = false;
+    }
 }
--- GameJamGame/Assets/Scripts/TentacleMovement.cs
+++ GameJamGame/Assets/Scripts/TentacleMovement.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class TentacleMovement : MonoBehaviour
 {
-
+    public bool stunned = false;
+    float stunDur = 3.0f;
     public string[] controls = new string[2];
     /*
      * 0 = Horizontal
@@ -26,30 +27,40 @@ public class TentacleMovement : MonoBehaviour
 
     }
     void FixedUpdate()
-    { 
-
-        float xValue = Input.GetAxis(controls[0]);
-        float yValue = Input.GetAxis(controls[1]);
-
-        Vector2 forwardVector = new Vector2 (xValue, yValue) + new Vector2 (transform.position.x, transform.position.y);
-        float dist = Vector2.Distance (forwardVector, playerBody.position);
-
-        if (rb2D.velocity.magnitude < 20 && dist < 4.5f)
+    {
+        if (!stunned)
         {
-            rb2D.AddForce(new Vector2(xValue, yValue) * 17, ForceMode2D.Force);
-        }
+            float xValue = Input.GetAxis(controls[0]);
+            float yValue = Input.GetAxis(controls[1]);
 
-        if (dist > 4)
+            Vector2 forwardVector = new Vector2(xValue, yValue) + new Vector2(transform.position.x, transform.position.y);
+            float dist = Vector2.Distance(forwardVector, playerBody.position);
+
+            if (rb2D.velocity.magnitude < 20 && dist < 4.5f)
+            {
+                rb2D.AddForce(new Vector2(xValue, yValue) * 17, ForceMode2D.Force);
+            }
+
+            if (dist > 4)
+            {
+
+                rb2D.drag = 7;
+
+            }
+            else
+            {
+                rb2D.drag = 1;
+            }
+        }
+        if (stunned)
         {
-
-            rb2D.drag = 7;
-
+            StartCoroutine(Stun());
         }
-        else
-        {
-            rb2D.drag = 1;
-        }
-
+    }
+    IEnumerator Stun()
+    {
+        yield return new WaitForSeconds(stunDur);
+        stunned = false;
     }
 
 }
