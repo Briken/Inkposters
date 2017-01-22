188d14bc6959e3588a8210d8cc8cb1f252694da1
Briken <brokenbriken@gmail.com>
2017-01-22 02:41:28 +0000

    Revert "pelican bomb"
    This reverts commit 49954e58489562963f9652fd8e3b1f9c92eefb58.

--- GameJamGame/Assets/Scripts/Pelicant.cs
+++ GameJamGame/Assets/Scripts/Pelicant.cs
@@ -54,14 +54,5 @@ public class Pelicant : MonoBehaviour {
         fracJourney = distCovered / journey1Length;
         transform.position = Vector2.Lerp(startPoint, target.transform.position, fracJourney);
     }
-    
-    void OnCollisionEnter2D(Collision2D squid)
-    {
-        Debug.Log("collided");
-        if (squid.gameObject.tag == "Player1" || squid.gameObject.tag == "Player2" || squid.gameObject.tag == "Hand1" || squid.gameObject.tag == "Hand2")
-        {
-            Destroy(this.gameObject);
-        }
-    }
 
 }
