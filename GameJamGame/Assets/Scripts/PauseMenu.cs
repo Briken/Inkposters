dcf68cf54c57ed977c2850ae91dc968e006c0ea4
Agleplop <arduncan111@gmail.com>
2017-01-28 13:44:16 +0000

    Menu Start

---
+++ GameJamGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour 
+{
+
+	public GameObject pauseMenuObj;
+
+	public GameObject resumeButton;
+	public GameObject settingsButton;
+	public GameObject quitButton;
+
+	public GameObject settingsMenu;
+
+	bool paused;
+
+	bool settings;
+
+	bool music;
+	bool sfx;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown ("player1A"))
+		{
+			if (!paused)
+				Pause ();
+			else
+				Resume ();
+		}
+	}
+
+	void Pause ()
+	{
+		paused = true;
+		pauseMenuObj.SetActive (true);
+		Time.timeScale = 0.0f;
+	}
+
+	public void Resume ()
+	{
+		pauseMenuObj.SetActive (false);
+		paused = false;
+		Time.timeScale = 1.0f;
+	}
+
+	public void Settings ()
+	{
+		
+		if (!settings)
+		{
+			resumeButton.SetActive = false;
+			settingsButton.SetActive = false;
+			quitButton.SetActive = false;
+
+			settings = true;
+		}
+
+	}
+
+	public void Quit ()
+	{
+
+	}
+
+	public void MusicToggle ()
+	{
+
+	}
+
+	public void SoundToggle ()
+	{
+
+	}
+
+}
